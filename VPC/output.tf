286a6e86db7bbc856d1f9ba365bc06ae9eb9deb8
veduushah <ved.kadhi@gmail.com>
2025-09-04 22:18:18 +0530

    Add VPC Terraform module as normal folder

---
+++ VPC/output.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  value = [for s in aws_subnet.public : s.id]
+}
+
+output "private_subnet_ids" {
+  value = [for s in aws_subnet.private : s.id]
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
---
+++ VPC/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  description = "Base name for tagging resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDRs for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDRs for private subnets"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Common tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
